d16761c9d5404f8cc8c86928945880fac747e610
Mukangafu <dannymuturi99@gmail.com>
2025-09-18 19:51:07 +0300

    Update role.js

--- role.js
+++ role.js
@@ -1,8 +1,5 @@
-module.exports = function(roles = []) {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ error: 'Access denied' });
-    }
-    next();
-  };
-};
+const auth = require('./middleware/auth');
+const role = require('./middleware/role');
+
+app.post('/api/doctors', auth, role(['admin']), createDoctor);
+app.get('/api/appointments', auth, role(['doctor','admin']), getAppointments);
